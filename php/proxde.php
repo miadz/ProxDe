51447a8377af919640579953068968734a514d0d
Andre Santos <NodePoint@users.noreply.github.com>
2017-07-31 03:17:23 +0100

    Clean up & info update
    Nothing to see here.

--- php/proxde.php
+++ php/proxde.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Project Name: ProxDe
-Version: 1.1
+Version: 1.1.1
 Type: Modular [PHP Function]
 Creator: Andre S.
-Github: CoolApps45
+Github: NodePoint
 THIS SCRIPT GOES THROUGH THE HEADERS AND CHECKS IF THERE IS A MATCH.
 THERE IS NO GUARANTEE THAT THIS WILL DETECT ALL PROXIES, IF THE DETECTION FAILS ON THE PROXY THEN IT HAS THOSE HEADERS HIDDEN.
 About Exclusions: Exclusions is basically a IP whitelist. You can either have it coded in or you can use a database of your choice to put in the list of IPs in array / list form.
@@ -17,85 +17,85 @@ At the end of the function, there's a couple of examples of how to use the real_
 */
 # FUNCTION START
 function real_ip($type = 1) {
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$count = 0; // Starting point for count
-	$excl = array(); // Exclusion list filler
-	$ext_m = $ip; // Output if no header matches found
-	$proxy_h = array(
-		'HTTP_VIA',
-		'VIA',
-		'HTTP_FORWARDED_FOR_IP',
-		'HTTP_X_FORWARDED_FOR',
-		'HTTP_FORWARDED_FOR',
-		'HTTP_X_FORWARDED',
-		'HTTP_FORWARDED',
-		'HTTP_CLIENT_IP',
-		'CLIENT_IP',
-		'X_FORWARDED_FOR',
-		'FORWARDED_FOR',
-		'X_FORWARDED',
-		'FORWARDED',
-		'FORWARDED_FOR_IP',
-		'HTTP_PROXY_CONNECTION'
-	); // Headers to go through (used for detection)
-	$e_ip = array(
-	'0.0.0.0'
-	); // IP exclusion list (client & header IP's), replace the placeholder IP with nothing or with an IP to exclude
-		$excl = array_merge((array)$e_ip, (array)$excl);
-	switch($type) {
-	   case 1:
-                # Detect if IP contains headers (TYPE_1)
-            if(!in_array($ip, $excl)) {
-                foreach($proxy_h as $x) {
-				    if(isset($_SERVER[$x]) && $_SERVER[$x] != $ip && !in_array($_SERVER[$x], $excl)) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $count = 0; // Starting point for count
+    $excl = array(); // Exclusion list filler
+    $ext_m = $ip; // Output if no header matches found
+    $proxy_h = array(
+        'HTTP_VIA',
+        'VIA',
+        'HTTP_FORWARDED_FOR_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_FORWARDED',
+        'HTTP_CLIENT_IP',
+        'CLIENT_IP',
+        'X_FORWARDED_FOR',
+        'FORWARDED_FOR',
+        'X_FORWARDED',
+        'FORWARDED',
+        'FORWARDED_FOR_IP',
+        'HTTP_PROXY_CONNECTION'
+    ); // Headers to go through (used for detection)
+    $e_ip = array(
+        '0.0.0.0'
+    ); // IP exclusion list (client & header IP's), replace the placeholder IP with nothing or with an IP to exclude
+    
+    $excl = array_merge((array)$e_ip, (array)$excl);
+
+    switch ($type) {
+       case 1:
+            # Detect if IP contains headers (TYPE_1)
+            if (!in_array($ip, $excl)) {
+                foreach ($proxy_h as $x) {
+                    if (isset($_SERVER[$x]) && $_SERVER[$x] != $ip && !in_array($_SERVER[$x], $excl)) {
                         $count += 1; // If on proxy
                     }
-				    elseif(!isset($_SERVER[$x]) || $_SERVER[$x] == $ip) {
+                    elseif (!isset($_SERVER[$x]) || $_SERVER[$x] == $ip) {
                         $count += 0; // If not detected on proxy
                     }
-			     }
-			}
-            else {
+                }
+            } else {
                 $count += 0; // Exclusion
             }
-            if($count > 0) {
-                return False; // If count variable is more than 0 (proxy hit)
-            }
-            else {
-                return True; // If there was 0 as a result (no proxy detected)
+            if ($count > 0) {
+                return false; // If count variable is more than 0 (proxy hit)
+            } else {
+                return true; // If there was 0 as a result (no proxy detected)
             }
             break;
         case 2:
             # Attempt to collect the potentially real IP (TYPE_2)
             $p_ip = $ext_m;
-            if(!in_array($ip, $excl)) {
-                foreach($proxy_h as $x){
-				    if(isset($_SERVER[$x]) && $_SERVER[$x] != $ip && $_SERVER[$x] != '' && !in_array($_SERVER[$x], $excl)) {
+            if (!in_array($ip, $excl)) {
+                foreach ($proxy_h as $x) {
+                    if (isset($_SERVER[$x]) && $_SERVER[$x] != $ip && $_SERVER[$x] != '' && !in_array($_SERVER[$x], $excl)) {
                         $p_ip = $_SERVER[$x]; // If on proxy
                     }
                 }
             }
-            if($p_ip == '') {
+            if ($p_ip == '') {
                 $p_ip = $ext_m; // Exclusion (main IP)
             }
             return $p_ip;
             break;
         default:
-            trigger_error("Invalid value set in first parameter", E_USER_WARNING); // Error if there's no valid parameter value specified
+            trigger_error('Invalid value set in first parameter', E_USER_WARNING); // Error if there's no valid parameter value specified
             return null;
             break;
-        }
+    }
 }
 # FUNCTION END
 
 echo '- - - - ProxDe - - - -';
 echo '<br>';
 echo '<br>';
-if(real_ip(1) === False) {
-	echo 'Your connection was found to be suspicious.'; // This should be obvious..
+if (real_ip(1) === false) {
+    echo 'Your connection was found to be suspicious.'; // This should be obvious..
 }
 else {
-	echo 'You\'ve passed detection.'; // Made it threw
+    echo 'You\'ve passed detection.'; // Made it threw
 }
 echo '<br>';
 echo real_ip(2);
